d72ae1ae8b67b02da4d86ea1e8306022c08d580d
Francisco Villar Salvador <franvlive@gmail.com>
2021-09-09 19:38:09 +0200

    Main Container - Basic structure and styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import Header from "./components/header";
+import "./app.scss";
 
 function App() {
-  return <Header></Header>;
+  return (
+    <section className="app">
+      <Header></Header>
+      <main className="main-content"></main>
+    </section>
+  );
 }
 
 export default App;
